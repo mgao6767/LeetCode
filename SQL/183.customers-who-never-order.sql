1455fab40858c6e736cdc0813468d9dc71387714
Adrian Gao <mingze.gao@sydney.edu.au>
2022-08-28 21:39:30 +1000

    Create 183.customers-who-never-order.sql

---
+++ SQL/183.customers-who-never-order.sql
@@ -0,0 +1,92 @@
+--
+-- @lc app=leetcode id=183 lang=mysql
+--
+-- [183] Customers Who Never Order
+--
+-- https://leetcode.com/problems/customers-who-never-order/description/
+--
+-- database
+-- Easy (66.38%)
+-- Likes:    1416
+-- Dislikes: 91
+-- Total Accepted:    441.2K
+-- Total Submissions: 662.9K
+-- Testcase Example:  '{"headers": {"Customers": ["id", "name"], "Orders": ["id", "customerId"]}, "rows": {"Customers": [[1, "Joe"], [2, "Henry"], [3, "Sam"], [4, "Max"]], "Orders": [[1, 3], [2, 1]]}}'
+--
+-- Table: Customers
+-- 
+-- 
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table indicates the ID and name of a customer.
+-- 
+-- 
+-- 
+-- 
+-- Table: Orders
+-- 
+-- 
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | customerId  | int  |
+-- +-------------+------+
+-- id is the primary key column for this table.
+-- customerId is a foreign key of the ID from the Customers table.
+-- Each row of this table indicates the ID of an order and the ID of the
+-- customer who ordered it.
+-- 
+-- 
+-- 
+-- 
+-- Write an SQL query to report all customers who never order anything.
+-- 
+-- Return the result table in any order.
+-- 
+-- The query result format is in the following example.
+-- 
+-- 
+-- Example 1:
+-- 
+-- 
+-- Input: 
+-- Customers table:
+-- +----+-------+
+-- | id | name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+-- Orders table:
+-- +----+------------+
+-- | id | customerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+-- Output: 
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+
+-- 
+-- 
+--
+-- @lc code=start
+SELECT name as Customers
+FROM Customers
+WHERE id not in (
+    SELECT DISTINCT customerId as id
+    FROM Orders
+  );
+-- @lc code=end
